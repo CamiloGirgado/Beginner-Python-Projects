d2b4b87b4d852d771043e0bc2407fddc69aaeaca
CamiloGirgado <camilogirgado@gmail.com>
2022-04-20 09:59:00 -0700

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,18 @@
+#Random Language
+# all vowels -> o 
+#for loop in combo with if loop
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "O"
+            else:
+                translation = translation + "o" 
+        else:
+            translation = translation + letter
+    return(translation)
+
+
+print(translate(input("Enter a phrase:")))
